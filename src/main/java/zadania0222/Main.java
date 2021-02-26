4e1e13466f97a9ebca4bbb4c01c5907d2d7231f5
Tomasz Wołyniec <t.wolyniec@tlen.pl>
2021-02-26 12:52:35 +0100

    skończone zadania domowe 26.02.2021

--- src/main/java/zadania0222/Main.java
+++ src/main/java/zadania0222/Main.java
@@ -6,8 +6,49 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        basketDemo();
+    }
 
+    private static void basketDemo() {
+        Basket basket = new Basket(9);
+        try {
+            basket.addToBasket();
+            basket.addToBasket();
+        } catch (BasketFullException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Basket basket2 = new Basket(1);
+        try {
+            basket2.removeFromBasket();
+            basket2.removeFromBasket();
+        } catch (BasketEmptyException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void threadsDemo() throws InterruptedException {
+        Thread thread = new MyThread("th", 100);
+        thread.start();
+
+        Thread thread2 = new MyThread("th --", 300);
+        thread2.start();
+
+        Thread thread3 = new Thread(new MyRunnable("th ----", 500));
+        thread3.start();
+
+        thread.join();
+        System.out.println("koniec wątku 1");
+
+        thread2.join();
+        System.out.println("koniec wątku 2");
+
+        thread3.join();
+        System.out.println("koniec programu");
+    }
+
+    private static void computerDemo() {
         Computer c1 = new Computer("Intel", "GeForce", "Dell", "1", 16, 2400);
         Computer c2 = new Computer("AMD", "GeForce", "Dell", "2", 8, 3000);
         Computer c3 = new Computer("AMD", "GeForce", "Dell", "3", 8, 2800);
@@ -25,7 +66,6 @@ public class Main {
         Laptop laptop = new Laptop("AMD", "GeForce", "Dell", "2", 8, 3000, 5000);
         System.out.println("--------------");
         System.out.println(laptop);
-
     }
 
     private static void sortListZADemo() {
